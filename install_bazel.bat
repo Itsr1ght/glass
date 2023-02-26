77a8961bb6292ab14d6b9ca49b540e5e628a8021
Bilal <bjlegend007@gmail.com>
2023-02-26 09:36:22 +0530

    Updated the install_bazel.bat file

--- install_bazel.bat
+++ install_bazel.bat
@@ -1,2 +1,7 @@
 @echo off
-curl -L https://github.com/bazelbuild/bazelisk/releases/download/v1.15.0/bazelisk-windows-amd64.exe --output bazelisk.exe 
+if exist bazelisk.exe (
+	echo already downloaded
+	bazelisk.exe
+) else (
+	curl -L https://github.com/bazelbuild/bazelisk/releases/download/v1.15.0/bazelisk-windows-amd64.exe --output bazelisk.exe 
+)
