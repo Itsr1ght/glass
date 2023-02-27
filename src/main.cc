f6b7fc1682185be0132dd826ab46a947021f6c8b
bilal <bjlegend007@gmail.com>
2023-02-27 21:21:46 +0530

    Added the Shader Class and Separate shader files

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 
+#include "Shader.h"
+
 void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -54,65 +56,6 @@ int main()
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 
-    // vertex shader code
-    const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "   ourColor = aColor;\n"
-    "}\0";
-
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    int  success_vertex;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success_vertex);
-
-    if(!success_vertex)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-
-    // fragment shader code
-    
-    const char *fragmentShaderSource = "#version 330 core\n"
-            "out vec4 FragColor;\n"
-            "in vec3 ourColor;\n"
-            "void main()\n"
-            "{\n"
-            "FragColor = vec4(ourColor,1.0);\n"
-            "}\0"; 
-
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    int success_fragment;
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success_fragment);
-
-    if(!success_fragment)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);  
     // 1. bind Vertex Array Object
@@ -127,8 +70,6 @@ int main()
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    glUseProgram(shaderProgram);
-
 
     unsigned int EBO;
     glGenBuffers(1, &EBO);
@@ -141,6 +82,8 @@ int main()
     glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
     std::cout << "Maximum number of vertex attributes supported: " << nrAttributes << std::endl;
 
+    Shader shader("./res/vertex_shader.glsl", "./res/fragment_shader.glsl");
+
     glViewport(0, 0, 800, 600);
     while(!glfwWindowShouldClose(window))
     {
@@ -150,9 +93,9 @@ int main()
         glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         //bind and uses the shader program
-        glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        shader.use();
         //draw a rectangle
         glDrawArrays(GL_TRIANGLES, 0, 3);
         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -161,9 +104,6 @@ int main()
         glfwPollEvents();
     }
 
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);  
-    glDeleteProgram(shaderProgram);
     glfwTerminate();
     return 0;
 }
