4d2d2fd860444f5c1b8b25d572cf942a8ed07339
bilal <bjlegend007@gmail.com>
2023-03-04 22:24:33 +0530

    adding separate window class

---
+++ src/window.cc
@@ -0,0 +1,29 @@
+#include "window.h"
+#include <glm/fwd.hpp>
+#include <string>
+
+Window::Window(){
+
+}
+
+
+void Window::setSize(glm::vec2& size) const{
+
+}
+
+glm::vec2 Window::getSize(){
+
+}
+
+void Window::setTitle(std::string title) const{
+
+}
+
+std::string Window::getTitle(){
+
+}
+
+
+Window::~Window(){
+
+}
