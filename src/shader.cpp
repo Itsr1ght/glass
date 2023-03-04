f53187f9fd43d0bbb5cc712932aa7a8e6ac4b3b2
bilal <bjlegend007@gmail.com>
2023-03-05 00:02:47 +0530

    refactoring shader cpp file

--- src/shader.cpp
+++ src/shader.cpp
@@ -96,19 +96,19 @@ namespace Glass {
     }
 
     void Shader::setBool(const std::string &name, bool value) const{
-            glUniform1i(glGetUniformLocation(ID, name.c_str()), (bool)value); 
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (bool)value); 
     }
 
     void Shader::setInt(const std::string &name, int value) const{
-            glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
     }
 
     void Shader::setFloat(const std::string &name, float value) const{
-            glUniform1i(glGetUniformLocation(ID, name.c_str()), (float)value); 
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (float)value); 
     }
 
     void Shader::setMat4(const std::string &name, glm::mat4 value) const{
-            glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
     }
 
 }
