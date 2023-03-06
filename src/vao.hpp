9192169660f7d890b57d6e02f21a5f262acf039f
bilal <bjlegend007@gmail.com>
2023-03-06 20:17:58 +0530

    added the vao class into project(not fully implimented)

---
+++ src/vao.hpp
@@ -0,0 +1,16 @@
+#ifndef VAO
+#define VAO
+
+namespace Glass {
+
+    class Vao{
+        public:
+            Vao();
+            ~Vao();
+        private:
+            unsigned int m_vao;
+    };
+
+}
+
+#endif
