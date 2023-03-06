5448876f0de1a38b841163aec2827da7614101ce
bilal <bjlegend007@gmail.com>
2023-03-06 20:18:32 +0530

    added the vbo class into project(not fully implimented)

---
+++ src/vbo.cpp
@@ -0,0 +1,14 @@
+#include "vbo.hpp"
+
+
+/* needs to impliment */
+namespace Glass {
+
+    Vbo::Vbo(){
+
+    }
+
+    Vbo::~Vbo(){
+    }
+
+}
---
+++ src/vbo.hpp
@@ -0,0 +1,16 @@
+#ifndef VBO
+#define VBO
+
+namespace Glass {
+
+    class Vbo{
+        public:
+            Vbo();
+            ~Vbo();
+        private:
+            unsigned int m_vbo;
+    };
+
+}
+
+#endif
