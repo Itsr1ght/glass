daffce0ff1ea80470eee1471d4af251596c8a526
bilal <bjlegend007@gmail.com>
2023-03-13 19:42:52 +0530

    changed the main func with new classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
+#include "raw_model.hpp"
+#include "renderer.hpp"
+#include "texture.hpp"
 #include "window.hpp"
 #include <glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <ostream>
-#include <string>
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 
 
 //glm headers
@@ -26,6 +24,7 @@ glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
 int main()
 {
     Window window({{800, 600}, "Hello World"});    
+    Renderer renderer(window);
     float vertices[] = {
         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
          0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -76,33 +75,8 @@ int main()
         1, 2, 3    // second triangle
     };
 
-    //create the array buffer object
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    //bind to array buffer
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    //add the vertices to vbo
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);  
-    // 1. bind Vertex Array Object
-    glBindVertexArray(VAO);
-
-   // position attribute
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // texture coord attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    RawModel model(vertices, indices);
+    renderer.add_to_renderer(model);
 
     //camera codes
     glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);  
@@ -115,29 +89,7 @@ int main()
     glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
 
 
-
-    //how many vertex attribute are supported
-    int nrAttributes;
-    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-    std::cout << "Maximum number of vertex attributes supported: " << nrAttributes << std::endl;
-
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load("./res/texture/container.jpg", &width, &height, &nrChannels, 0);
-
-    unsigned int texture;
-    glGenTextures(1, &texture);  
-
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    if (data){
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else {
-        std::cout << "Failed to Load Texture" << std::endl;
-    }
-    stbi_image_free(data);
+    Texture texture("./res/texture/container.jpg");
 
     Shader shader("./res/shaders/vertex_shader.glsl", "./res/shaders/fragment_shader.glsl");
 
@@ -145,9 +97,6 @@ int main()
     while(!window.IsWindowShouldClose())
     {
 	window.ClearColor();
-        //bind and uses the shader program
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        shader.use();
         shader.setInt("texture1", 0);
         // create transformations
         glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
@@ -175,9 +124,7 @@ int main()
         // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
         // process the events in window 
         shader.setMat4("projection", projection);
-        glBindTexture(GL_TEXTURE_2D, texture);
-        glBindVertexArray(VBO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        renderer.render();
         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         // swapping the buffer
 	window.SwapBuffer();
